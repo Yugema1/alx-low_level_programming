307d14ad1d1845202a3564ce1471eed3467d6c5a
EWUZIEM EMMANUEL UGOCHUKWU <mmnlugochukwu@gmail.com>
2023-06-06 15:31:13 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char y;
+
+	y = 'a';
+	while
+		(y <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
