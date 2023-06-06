731799fe5df1f84a27f3fb69402883d5b2728535
EWUZIEM EMMANUEL UGOCHUKWU <mmnlugochukwu@gmail.com>
2023-06-06 15:20:41 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, y);
 	if (y == 0)
 		printf("Last digit of %d is %d and is 0\n", n, y);
-	if (m < 6 && y != 0)
+	if (y < 6 && y != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 	return (0);
 }
