9ab2a7bdde215e65cc0afa5f013a0cb07e774f6e
EWUZIEM EMMANUEL UGOCHUKWU <mmnlugochukwu@gmail.com>
2023-06-06 15:04:22 -0700

    updated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,21 +3,21 @@
 #include <stdio.h>
 
 /**
- * main - A program that will assign a radom number to the variable n each time it is executed.
+ * main - Entry Point.
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	
-	return 0;
+	return (0);
 }
